f3af47aa62cdbf39766472f13a691f4bf21aa1ef
NaveenPrasanth-M <113746926+NaveenPrasanth-M@users.noreply.github.com>
2022-11-23 20:41:30 +0530

    Add files via upload

---
+++ Project-3/springboot/src/main/java/net/javaguides/springboot/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package net.javaguides.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.springboot.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+}
