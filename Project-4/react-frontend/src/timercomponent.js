226ee147cc95432415e1c5dc03e37702393e4201
NaveenPrasanth-M <113746926+NaveenPrasanth-M@users.noreply.github.com>
2022-11-23 20:44:52 +0530

    Add files via upload

---
+++ Project-4/react-frontend/src/timercomponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Timer extends Component(){
+    render(){
+        return (
+            <div>
+               {10}
+            </div>
+       )
+    }
+}
+export default Timer
